4ba14874af2213b21360aec6cc371c2454b2878b
Zbigniew Piotrowski <22048166+piotrows@users.noreply.github.com>
2026-02-05 22:27:41 +0100

    FA added to fetchcontent

--- cmake/ecland_fetchcontent_ecbuild.cmake
+++ cmake/ecland_fetchcontent_ecbuild.cmake
@@ -8,8 +8,8 @@ FetchContent_Populate(
   )
 find_package( ecbuild 3.11 REQUIRED HINTS ${CMAKE_BINARY_DIR} )
 FetchContent_Populate(
-    ecbuild
-    URL            https://github.com/ecmwf/ecbuild/archive/refs/tags/3.11.0.tar.gz
+    field_api 
+    URL            https://github.com/ecmwf-ifs/field_api/archive/refs/tags/v0.3.7.tar.gz 
     SOURCE_DIR     ${CMAKE_BINARY_DIR}/ecbuild
     BINARY_DIR     ${CMAKE_BINARY_DIR}/_deps/ecbuild-build
     SUBBUILD_DIR   ${CMAKE_BINARY_DIR}/_deps/ecbuild-subbuild
