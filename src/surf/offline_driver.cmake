3abeeae7d398ca7c85b61cf01702440468a99cf8
Zbigniew Piotrowski <22048166+piotrows@users.noreply.github.com>
2026-02-05 12:33:22 +0100

    Updiag now accepts derived types

--- src/surf/offline_driver.cmake
+++ src/surf/offline_driver.cmake
@@ -20,6 +20,7 @@ ecbuild_generate_fortran_interfaces(
 list(APPEND offline_driver_src
     buffer_utils.F90
     callpar1s.F90
+    callpar1s_layer.F90
     cnt01s.F90
     cnt21s.F90
     cnt31s.F90
@@ -73,6 +74,7 @@ list(APPEND offline_driver_src
     suvdfs.F90
     updcal.F90
     upddiag.F90
+    upddiag_layer.F90
     updtim1s.F90
     vdfdifc.F90
     vdfdifh1s.F90
@@ -117,7 +119,6 @@ list(APPEND offline_driver_src
     yomlog1s.F90
     yomlun1s.F90
     yomrip.F90
-    callpar1s_layer.F90
 )
 list(TRANSFORM offline_driver_src PREPEND offline/driver/)
 
