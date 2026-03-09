4ce4d2678634341ca4d3b566047f94ec6500a77c
Zbigniew Piotrowski <22048166+piotrows@users.noreply.github.com>
2026-03-09 18:06:17 +0100

    Strip eccodes dependency from unnecessary components

--- cmake/ecland_fetchcontent_dependencies.cmake
+++ cmake/ecland_fetchcontent_dependencies.cmake
@@ -11,13 +11,20 @@ if (HAVE_FETCHCONTENT_DEPENDENCIES)
 #### eccodes
 FetchContent_Declare(
   eccodes
-  URL https://github.com/ecmwf/eccodes/archive/refs/tags/2.44.0.tar.gz
+  URL https://github.com/ecmwf/eccodes/archive/refs/tags/2.46.0.tar.gz
   FIND_PACKAGE_ARGS
 )
 set( ECCODES_ENABLE_MEMFS ON )
 set( ECCODES_ENABLE_TESTS OFF )
 set( ECCODES_ENABLE_JPG OFF )
 set( ECCODES_ENABLE_PNG OFF )
+set( ECCODES_ENABLE_NETCDF OFF )
+set( ECCODES_ENABLE_JPG_LIBJASPER OFF )
+set( ECCODES_ENABLE_JPG_LIBOPENJPEG OFF )
+set( ECCODES_ENABLE_GEOGRAPHY OFF )
+set( ECCODES_ENABLE_EXAMPLES OFF )
+set( ECCODES_ENABLE_PRODUCT_GRIB OFF )
+set( ECCODES_ENABLE_AEC OFF )
 
 
 #### fiat
@@ -29,7 +36,15 @@ FetchContent_Declare(
 set( FIAT_ENABLE_TESTS OFF )
 set( FIAT_ENABLE_DR_HOOK_NVTX OFF )
 
-FetchContent_MakeAvailable(eccodes fiat) # Internally calls find_package() first
+#### field api
+FetchContent_Declare(
+    field_api 
+    URL            https://github.com/ecmwf-ifs/field_api/archive/refs/tags/v0.3.7.tar.gz 
+    FIND_PACKAGE_ARGS
+)
+set( FIELD_API_ENABLE_TESTS OFF )
+
+FetchContent_MakeAvailable(eccodes fiat field_api) # Internally calls find_package() first
 
 endif()
 
