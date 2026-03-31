c28dfd0c765027a994a62692f8ff9be6fac9b2cd
Ahmad Nawab <ahmad.nawab@ecmwf.int>
2026-03-31 11:18:04 +0100

    cmake: delete fetchcontent macros

--- cmake/ecland_fetchcontent_ecbuild.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-FetchContent_Populate(
-    ecbuild
-    URL            https://github.com/ecmwf/ecbuild/archive/refs/tags/3.11.0.tar.gz
-    SOURCE_DIR     ${CMAKE_BINARY_DIR}/ecbuild
-    BINARY_DIR     ${CMAKE_BINARY_DIR}/_deps/ecbuild-build
-    SUBBUILD_DIR   ${CMAKE_BINARY_DIR}/_deps/ecbuild-subbuild
-  )
-find_package( ecbuild 3.11 REQUIRED HINTS ${CMAKE_BINARY_DIR} )
